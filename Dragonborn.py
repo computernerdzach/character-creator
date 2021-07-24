ec0ecca6376c96c63f13d22a13c245edfb18d1ea
Andrew Galloway <agalloway@coppertreeanalytics.com>
2021-07-24 15:01:10 -0700

    removed Character inheritence and implemented abstract class

--- Dragonborn.py
+++ Dragonborn.py
@@ -1,12 +1,49 @@
-import character
+from abc import ABC, abstractmethod
 
 
-class Dragonborn(character.Character):
+class Race(ABC):
+    def __init__(self):
+        self.score_modifiers = {
+            'STR': 0,
+            'DEX': 0,
+            'INT': 0,
+            'WIS': 0,
+            'CHA': 0,
+            'CON': 0
+        }
+        self.languages = ['common']
 
-    def __init__(self, name):
-        super().__init__(name)
-        self.STR += 2
-        self.CHA += 1
+    @property
+    @abstractmethod
+    def size(self):
+        pass
+
+    @property
+    @abstractmethod
+    def speed(self):
+        pass
+
+    @property
+    @abstractmethod
+    def age_range(self):
+        pass
+
+    @property
+    @abstractmethod
+    def traits(self):
+        pass
+
+
+class Dragonborn(Race):
+
+    def __init__(self):
+        super().__init__()
+        self.score_modifiers['STR'] += 2
+        self.score_modifiers['CHA'] += 1
+        self.languages.append('draconic')
+
+    def __str__(self):
+        return 'Dragonborn'
 
     @property
     def size(self):
@@ -18,12 +55,8 @@ class Dragonborn(character.Character):
 
     @property
     def age_range(self):
-        return 15, 80
-
-    @property
-    def languages(self):
-        return 'common', 'draconic'
+        return [15, 80]
 
     @property
     def traits(self):
-        return 'draconic-ancestry', 'breath-weapon', 'damage-resistance'
+        return ['draconic-ancestry', 'breath-weapon', 'damage-resistance']
